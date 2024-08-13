c12294c710365521197ebe7693a491b96dd9b15f
Hayden Tedrake <haydentedrake@gmail.com>
2024-08-13 13:22:49 -0400

    finetuning yolov8 on sonar data with manually produced labels!

--- image_detector/scripts/yolo_detector2.py
+++ image_detector/scripts/yolo_detector2.py
@@ -1,17 +1,14 @@
 import ultralytics
 from ultralytics import YOLO
 from IPython.display import Image, display
+import os
+import glob
 
 # Check the installed version of ultralytics
 ultralytics.checks()
 
-# Mount Google Drive (if running in Google Colab)
-from google.colab import drive
-drive.mount('/content/drive')
-
-# Change the directory to your YOLOv8 directory
-import os
-os.chdir('/content/drive/MyDrive/YOLOv8')
+# Change to YOLOv8 directory
+os.chdir('/home/haydentedrake/sonar_data')
 
 # List files in the current directory
 print(os.listdir())
@@ -23,18 +20,23 @@ os.system('yolo task=detect mode=train model=yolov8s.pt data=data.yaml epochs=25
 print(os.listdir('runs/detect/train/'))
 
 # Display training results
-display(Image(filename='runs/detect/train/confusion_matrix.png', width=600))
-display(Image(filename='runs/detect/train/results.png', width=600))
-display(Image(filename='runs/detect/train/val_batch0_pred.jpg', width=600))
-
-# Validate the model
+try:
+    display(Image(filename='runs/detect/train/confusion_matrix.png', width=600))
+    display(Image(filename='runs/detect/train/results.png', width=600))
+    display(Image(filename='runs/detect/train/val_batch0_pred.jpg', width=600))
+except Exception as e:
+    print(f"Display error: {e}")
+#
+#  Validate the model
 os.system('yolo task=detect mode=val model=runs/detect/train/weights/best.pt data=data.yaml')
 
 # Predict using the trained model
-os.system('yolo task=detect mode=predict model=runs/detect/train/weights/best.pt conf=0.25 source=/content/drive/MyDrive/YOLOv8/test/images')
+os.system('yolo task=detect mode=predict model=runs/detect/train3/weights/best.pt conf=0.25 source=/content/drive/MyDrive/YOLOv8/test/images')
 
 # Display prediction results
-import glob
-for image_path in glob.glob('runs/detect/predict/*.jpg')[:3]:
-    display(Image(filename=image_path, width=600))
-    print("\n")
+try:
+    for image_path in glob.glob('runs/detect/predict/*.jpg')[:3]:
+        display(Image(filename=image_path, width=600))
+        print("\n")
+except Exception as e:
+    print(f"Display error: {e}")
